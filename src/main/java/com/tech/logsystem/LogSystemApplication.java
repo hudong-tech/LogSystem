386428e96da8d4786bead08860d8f2f3ed176a76
phil <hudong.tech@gmail.com>
2023-08-31 23:22:20 +0800

    init

---
+++ src/main/java/com/tech/logsystem/LogSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tech.logsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tech/logsystem/LogSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.logsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
